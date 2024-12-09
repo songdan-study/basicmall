39d61f8a9c5045d29b408c888fa6d256761a6708
YoonDanA <126229670+YoonDanA@users.noreply.github.com>
2024-12-09 23:58:13 +0900

    Feature : 상품 - 쿼리 어노테이션과 JPQL을 이용한 조회 테스트

--- basicmall/src/main/java/com/songdan/basicmall/item/ItemRepository.java
+++ basicmall/src/main/java/com/songdan/basicmall/item/ItemRepository.java
@@ -2,6 +2,8 @@ package com.songdan.basicmall.item;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
@@ -9,4 +11,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceGreaterThanEqualOrderByPriceDesc(int price);
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
