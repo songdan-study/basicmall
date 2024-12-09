93a1464bbf01f51a03d47a7c203868adb2da6f67
song eojin <feshed528@gmail.com>
2024-12-09 21:32:43 +0900

    Init : 스프링부트 애플리케이션 초기 환경설정

---
+++ basicmall/src/main/java/com/songdan/basicmall/BasicmallApplication.java
@@ -0,0 +1,13 @@
+package com.songdan.basicmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicmallApplication.class, args);
+	}
+
+}
---
+++ basicmall/src/test/java/com/songdan/basicmall/BasicmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songdan.basicmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
